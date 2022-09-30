2b393135b69b0c879e2dc2ea13c0afb2f4d0bd1c
Martin DE LAGARDE <mde-laga@e1r8p14.clusters.42paris.fr>
2022-09-30 16:04:23 +0200

    changed structure, skeleton server

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server Red-Tetris");
+});
+
+app.listen(3000, () => {
+  console.log(`[server]: Server is running at https://localhost:3000`);
+});
