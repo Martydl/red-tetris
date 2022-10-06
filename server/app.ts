b311832b5eef39e3ec870146d3852f177788f19d
mde-laga <mde-laga@PC-MARTIN.localdomain>
2022-10-06 09:45:04 +0200

    fixed client bundle, changed exec scripts & dependencies

--- server/app.ts
+++ server/app.ts
@@ -2,13 +2,14 @@ import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 
+app.use(express.static("client"));
 app.get("/", (req: Request, res: Response) => {
   // res.send("Express + TypeScript Server Red-Tetris");
   res.sendFile("client/index.html", { root: "." });
 });
 
 app.listen(3000, () => {
-  console.log(`[server]: Server is running at https://localhost:3000`);
+  console.log(`[server]: Server is running at http://localhost:3000`);
 });
 
 export const viteNodeApp = app;
