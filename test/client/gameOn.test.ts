9df7727955ab1003e999c941bc9ad08ae37891e7
AntoineMau <antoinemauffret@gmail.com>
2022-11-18 12:25:39 +0100

    supp old file

--- test/client/gameOn.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, it, Environment } from "vitest";
-import GameOn from "../../client/src/game/GameOn";
-
-it("GameOn", () => {
-  console.log(KeyboardEvent);
-  // expect(<GameOn/>, toBeDefined();
-});
